ac73f4de792e7132eafb6569b31a33f60d0db058
Okundolor Mondris <justmondris@gmail.com>
2020-10-17 00:20:50 +0100

    initial commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../Images/logo.png";
+const NavBar = () => {
+
+    return (
+        <div id="navbar">
+            <img id="logo" src={logo}  alt="spotify logo"/>
+            <p>hello</p>
+        </div>
+    )
+}
+
+
+export default NavBar;
